9e3bafa8212c8800c303de7935f6d578546c66d5
Eyad Ahmed <Eyad932004@outlook.com>
2024-11-12 19:48:44 +0200

    [0002]-hero & grid sections

---
+++ components/Hero.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Spotlight } from "./ui/SpotLight";
+import { GridBackground } from "./ui/BackGround";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import Link from "next/link";
+import { FaLocationArrow } from "react-icons/fa";
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80  h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <GridBackground />
+      <div className="flex justify-center relative my-20 z-10 ">
+        <div className="max-w-[80vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web MAgic with Next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into seamless User Experiences"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Eyad, a Front-end Developer based in saudi Arabia
+          </p>
+          <Link href="#about">
+            <MagicButton
+              title="show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
